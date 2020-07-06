b4997d4851587d9c4a9820af7e6ed555150f55a9
arthuralcorreia <arthurac1337@hotmail.com>
2020-07-06 11:20:39 -0300

    Commit da ultima mexida no código

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-const  CardContato = document.querySelectorAll(".LogoContato")
+const teste = document.querySelector(".LogoContato");
+
+const memoria = teste
+teste.addEventListener("mouseenter", function (){
+    this.innerHTML = "<p>Automações<br><br>Ciencia de Dados<br><br>ciencia de dados<p>"
+})
+teste.addEventListener("mouseleave", function (){
+    this.innerHTML = memoria
+})
