7cc0d4f71b442b9bc7b1effe8473b354f5001936
arthuralcorreia <arthurac1337@hotmail.com>
2020-07-06 22:59:19 -0300

    Inserçao das interaçoes no card de contato e do formulario

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,10 @@
 const LogoContato = document.getElementsByClassName("Linguagens");
+const Buttons = document.getElementById("CVDownloadButtons")
+const cardDownload = document.getElementById("cardDownload")
+const FormButton = document.getElementById("BotaoFormulario")
+const GridDetalhes = document.getElementById("GridDetalhesPessoais")
+const formularioDeContato = document.getElementById("formularioDeContato")
+const backForm = document.getElementById("backButton")
 
 for(var i = 0; i < LogoContato.length; i++){
     LogoContato[i].addEventListener("mouseover", function () {
@@ -13,3 +19,26 @@ for(var i = 0; i < LogoContato.length; i++){
         this.getElementsByTagName("img")[0].style.display = ""
     });
 }
+
+cardDownload.addEventListener("mouseover", function () {
+    this.getElementsByTagName("img")[0].style.display = "none"
+    Buttons.style.display = "grid"
+})
+
+cardDownload.addEventListener("mouseleave", function () {
+    this.getElementsByTagName("img")[0].style.display = "grid"
+    Buttons.style.display = "none"
+})
+
+FormButton.addEventListener("click", function () {
+    GridDetalhes.style.display = "none"
+    formularioDeContato.style.display = "grid"
+})
+
+backForm.addEventListener("click", () => {
+    GridDetalhes.style.display = "grid"
+    formularioDeContato.style.display = "none"
+})
+
+
+console.log(backForm)
